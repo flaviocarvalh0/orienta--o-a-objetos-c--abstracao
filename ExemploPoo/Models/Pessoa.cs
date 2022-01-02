3a6bcffc856aa722bc75a13809e8e678e93f8652
flaviocarvalh0 <flavioalvespw@outlook.com>
2022-01-02 12:06:22 -0300

    first commit

---
+++ ExemploPoo/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+using System;
+namespace ExemploPoo.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos");
+        }
+
+        public void podeDirigir()
+        {
+            if(Idade > 18)
+            {
+                Console.WriteLine($"{Nome}, você já pode tirar a carteira de habilitação");
+            }else{
+                Console.WriteLine($"Carteira de habilitação disponivel apos os 18 ano");
+            }
+        }
+    }
+}
---
+++ ExemploPoo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ExemploPoo.Models;
+
+namespace ExemploPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            Console.Write("Informe um nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Informe a idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            p1.Apresentar();
+            p1.podeDirigir();
+        }
+    }
+}
