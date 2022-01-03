eb592f5c2afedbd060cc10703befa1009b6af333
flaviocarvalh0 <flavioalvespw@outlook.com>
2022-01-03 11:17:14 -0300

    encapsulamento

--- ExemploPoo/Program.cs
+++ ExemploPoo/Program.cs
@@ -7,16 +7,63 @@ namespace ExemploPoo
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
 
-            Console.Write("Informe um nome: ");
-            p1.Nome = Console.ReadLine();
+            string opcao = Opcao();
+            while (opcao != "5")
+            {
+
+                switch (opcao)
+                {
+                    case "1":
+                        retangulo r = new retangulo();
+                        Console.Write("Digite um número: ");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Write("Digite um outro: ");
+                        double b = double.Parse(Console.ReadLine());
+                        r.DefinirMedidas(a, b);
+                        System.Console.WriteLine($"Área: {r.ObterArea()}");
+                        break;
+                    case "2":
+                        Pessoa p1 = new Pessoa();
+                        Console.Write("Informe um nome: ");
+                        p1.Nome = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Informe a idade: ");
+                        p1.Idade = int.Parse(Console.ReadLine());
+                        p1.Apresentar();
+                        p1.podeDirigir();
+                        break;
+                    case "3":
+                        
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcao = Opcao();
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+        private static string Opcao()
+        {
             Console.WriteLine();
-            Console.Write("Informe a idade: ");
-            p1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("1 - Encapsulamento");
+            Console.WriteLine("2 - abstração");
+            Console.WriteLine("5 - sair");
 
-            p1.Apresentar();
-            p1.podeDirigir();
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+
         }
     }
 }
